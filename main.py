ca0c89991c76d3157228cfcd2c7586847f23fde6
goranaviani <goranaviani@gmail.com>
2020-12-19 12:50:37 +0100

    second commit

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 # This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def print_hi():
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, this is check_kwargs')  # Press ⌘F8 to toggle the breakpoint.
